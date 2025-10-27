f6f7d6d2b6202eac2638c1502be9b1018686b627
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-10-27 08:59:59 +0200

    fix: Correct issuer URL in client.php for OIDC Discovery

--- client.php
+++ client.php
@@ -18,7 +18,7 @@ try {
         'clientId'     => 'test_client_123',
         'clientSecret' => 'test_secret_key',
         'redirectUri'  => 'http://127.0.0.1:8081/client.php',
-        'issuer'       => 'http://127.0.0.1:8010/xauth',
+        'issuer'       => 'http://xauth-server.com',
     ]);
 } catch (\RuntimeException $e) {
     $pageTitle = 'Error';
